28c8665107eed58cf89ff5e7b01f5a36b7d3d75e
Pablo Blázquez <91126465+Procoro@users.noreply.github.com>
2022-11-04 22:17:54 +0100

    Update ejercicio5.java

--- Pedro_Sarasúa_Cerezo/tarea.numero.cinco/src/main/java/tarea/numero/cinco/ejercicio5.java
+++ Pedro_Sarasúa_Cerezo/tarea.numero.cinco/src/main/java/tarea/numero/cinco/ejercicio5.java
@@ -5,6 +5,7 @@ public class ejercicio5 {
 	public static void main(String[] args) {
 		System.out.println("pedro estuvo aqui");
 		System.out.println("Raúl estuvo aquí.");
+		System.out.println("Pablo estuvo aqui");
 	}
 
 }
